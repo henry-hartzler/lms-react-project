2eb467e31659c2819659d568d48c3271aef3f694
Henry Hartzler <hartzler.henry@gmail.com>
2022-10-07 19:53:54 -0600

    added Bootstrap cdn to head tag in index

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,21 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+import { useState } from "react";
+import { Container, Row, Col, Button, Badge } from "reactstrap";
 
-export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
-
-  const incrementValue = Number(incrementAmount) || 0;
+const Counter = () => {
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
-  );
+    <Container>
+      <Row>
+        <Col>
+          <Button className="m-3" onClick={() => setCount(count - 1)}>-</Button>
+            <Badge color="success" className="m-3 p-3">{count}</Badge>
+          <Button className="m-3" onClick={() => setCount(count + 1)}>+</Button>
+        </Col>
+      </Row>
+      
+    </Container>
+    );
 }
+ 
+export default Counter;
