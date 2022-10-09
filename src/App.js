2921b837a6af6fc8685e154f51eefd770bbb5ddb
Talha Hisham <talhahisham92@gmail.com>
2022-10-09 16:35:28 -0500

    adding app again but its empty at the moment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
->>>>>>> e8a6c07ff29f582dadf06209aa18e3dcef1fd266
 import './App.css';
 import RewardStudents from './pages/RewardStudents';
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="App">
-=======
-    <div className='App'>
-      <RewardStudents />
->>>>>>> e8a6c07ff29f582dadf06209aa18e3dcef1fd266
     </div>
   );
 }
