8217550f3badec2016a823cc9d07a81ecd5a3a7a
Henry Hartzler <hartzler.henry@gmail.com>
2022-10-09 10:29:06 -0600

    Delete container in counter

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -6,21 +6,18 @@ const Counter = () => {
 
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Button outline color='primary' className="m-3" onClick={() => setCount(count - 1)}>-</Button>
-            <Badge 
-              color={count === 0 ? 'secondary' : count < 0 ? 'danger' : 'success'} 
-              className="m-3 p-3"
-            >
-                {count}
-            </Badge>
-          <Button outline color='primary' className="m-3" onClick={() => setCount(count + 1)}>+</Button>
-        </Col>
-      </Row>
-      
-    </Container>
+    <Row>
+      <Col>
+        <Button outline color='primary' className="m-3" onClick={() => setCount(count - 1)}>-</Button>
+          <Badge 
+            color={count === 0 ? 'secondary' : count < 0 ? 'danger' : 'success'} 
+            className="m-3 p-3"
+          >
+              {count}
+          </Badge>
+        <Button outline color='primary' className="m-3" onClick={() => setCount(count + 1)}>+</Button>
+      </Col>
+    </Row>
     );
 }
  
