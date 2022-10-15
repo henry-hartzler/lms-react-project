738f131fe3e3f73c14be8ba0226e93d8a8354f63
Henry Hartzler <hartzler.henry@gmail.com>
2022-10-14 18:43:05 -0600

    initial setup complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer'; 
+import HomePage from './pages/HomePage';
 import MarketPlace from './pages/MarketPlace';
 import RewardStudents from './pages/RewardStudents';
 import './App.css';
@@ -7,8 +9,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <RewardStudents />
-      <MarketPlace />
+      <Header />
+      <Routes>
+        <Route path = '/' element={<HomePage />} />
+        <Route path = 'marketplace' element={<MarketPlace />} />
+        <Route path = 'rewardstudents' element={<RewardStudents />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
