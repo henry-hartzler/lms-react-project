64c4b13698a0a2c477784ba36ec7ee8546557b75
Henry Hartzler <hartzler.henry@gmail.com>
2022-10-16 13:23:09 -0600

    studentsSlice

--- src/features/students/studentsSlice.js
+++ src/features/students/studentsSlice.js
@@ -1,4 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { STUDENTS } from "../../app/shared/STUDENTS";
+
+const initialState = {
+    studentsArray: STUDENTS
+}
+
+const studentsSlice = createSlice({
+    name: 'students',
+    initialState,
+    reducers: {
+        addStudent: (state, action) => {
+            const newStudent = {
+                id: state.studentsArray.length + 1,
+                ...action.payload
+            }
+            state.studentsArray.push(newStudent)
+        }
+    }
+})
+
+export const studentsReducer = studentsSlice.reducer
+
+export const { addStudent } = studentsSlice.actions
 
 export const selectAllStudents = () => STUDENTS;
 
