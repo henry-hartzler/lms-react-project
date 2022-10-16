c456b80d5a55a7593af26602a899f016a5c5521f
Henry Hartzler <hartzler.henry@gmail.com>
2022-10-16 14:23:18 -0600

    short circuit logic for studentList

--- src/features/students/StudentsList.jsx
+++ src/features/students/StudentsList.jsx
@@ -6,21 +6,30 @@ import { selectAllStudents } from "./studentsSlice";
 const StudentsList = () => {
     const students = useSelector(selectAllStudents);
 
+    if (students && students.length > 0) {
+        return (
+            <Row className="ms-auto">
+                {students.map(student => {
+                    return (
+                        <Col
+                            md='3'
+                            className="m-4"
+                            key={student.id}
+                        >
+                            <StudentCard student={student} />
+                        </Col>
+                    )
+                })}
+            </Row>
+        );
+    }
+
     return (
-        <Row className="ms-auto">
-            {students.map(student => {
-                return (
-                    <Col
-                        md='3'
-                        className="m-4"
-                        key={student.id}
-                    >
-                        <StudentCard student={student} />
-                    </Col>
-                )
-            })}
+        <Row className="m-4">
+            <h1>There are currently no students registered.</h1>
         </Row>
     );
+    
 }
  
 export default StudentsList;
