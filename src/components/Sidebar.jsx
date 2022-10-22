df2f180b45815a102fd46fdbe5494256b407803e
Henry Hartzler <hartzler.henry@gmail.com>
2022-10-21 23:59:49 -0600

    changed the order of students and marketplace

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,20 +6,20 @@ export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
         <NavLink key="0" className={styles.sideitem} to="/">
-            <i className="fa-solid fa-house"></i>
+            <i className="fa-solid fa-house fa-lg"></i>
             <span className={styles.linkText}>Home</span>
         </NavLink>
         <NavLink key="1" className={styles.sideitem} to="courses">
-            <i className="fa-solid fa-list"></i>
+            <i className="fa-solid fa-list fa-lg"></i>
             <span className={styles.linkText}>Courses</span>
         </NavLink>
-        <NavLink key="2" className={styles.sideitem} to="marketplace">
-            <i className="fa-solid fa-store"></i>
-            <span className={styles.linkText}>Marketplace</span>
-        </NavLink>
-        <NavLink key="3" className={styles.sideitem} to="students">
-            <i className="fa-solid fa-users"></i>
+        <NavLink key="2" className={styles.sideitem} to="students">
+            <i className="fa-solid fa-users fa-lg"></i>
             <span className={styles.linkText}>Students</span>
+        </NavLink>
+        <NavLink key="3" className={styles.sideitem} to="marketplace">
+            <i className="fa-solid fa-store fa-lg"></i>
+            <span className={styles.linkText}>Marketplace</span>
         </NavLink>
     </div>
   )
