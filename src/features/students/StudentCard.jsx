241b40bef52df4decfab993759891cb87639fb99
Henry Hartzler <hartzler.henry@gmail.com>
2022-10-29 20:19:41 -0600

    need to change id's so they are unique

--- src/features/students/StudentCard.jsx
+++ src/features/students/StudentCard.jsx
@@ -2,7 +2,7 @@ import { Card, CardImg, CardTitle } from "reactstrap";
 import Counter from "../counter/Counter";
 
 const StudentCard = ({ student }) => {
-    const {id, img, name, points} = student;
+    const {id, img, name} = student;
 
     // right now, name spacing is off when shrinking screens
     // make better responsive
@@ -16,7 +16,7 @@ const StudentCard = ({ student }) => {
                 top
             />
             <CardTitle className="m-3" tag='h1'>{name}</CardTitle>
-            <Counter key={id} id={id} points={points} />
+            <Counter key={id} id={id} />
         </Card>
       );
 }
